fa41439b540ded8436f5804f23690dc430b110fc
janmarsino98 <wiskyscapit@gmail.com>
2024-07-14 09:54:55 +0200

    Created job section

--- frontend/src/components/organisms/JobRow.jsx
+++ frontend/src/components/organisms/JobRow.jsx
@@ -3,7 +3,7 @@ import JobCard from "../molecules/JobCard";
 let count = 0;
 const JobRow = ({ jobCards }) => {
   return (
-    <div className="flex flex-row p-[16px]">
+    <div className="flex flex-wrap p-[16px] gap-[12px]">
       {jobCards.map((card, index) => {
         return (
           <JobCard
@@ -11,7 +11,7 @@ const JobRow = ({ jobCards }) => {
             title={card.title}
             minSalary={card.minSalary}
             maxSalary={card.maxSalary}
-            imgSrc={card.imgSrcw}
+            imgSrc={card.imgSrc}
           ></JobCard>
         );
       })}
--- frontend/src/components/pages/Landing.jsx
+++ frontend/src/components/pages/Landing.jsx
@@ -2,23 +2,78 @@ import React from "react";
 import Navbar from "../molecules/Navbar";
 import SearchBox from "../molecules/SearchBox";
 import JobCard from "../molecules/JobCard";
+import JobRow from "../organisms/JobRow";
 
 const Landing = () => {
   return (
     <div>
-      <div className="font-sans flex flex-col">
+      <div className="font-sans flex flex-col items-center">
         <Navbar></Navbar>
-        <div className="flex flex-col mx-[160px] my-[20px]">
+        <div className="flex flex-col max-w-[960px] my-[20px]">
           <h1 className="text-[32px] font-bold ml-[16px] mb-[16px]">
             Find Jobs Near You
           </h1>
           <SearchBox></SearchBox>
-          <JobCard
-            title="Cleaner"
-            imgSrc="https://images.pexels.com/photos/313715/pexels-photo-313715.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            minSalary={1000}
-            maxSalary={2000}
-          ></JobCard>
+          <JobRow
+            jobCards={[
+              {
+                title: "cleaner",
+                imgSrc:
+                  "https://images.pexels.com/photos/4239130/pexels-photo-4239130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "waiter",
+                imgSrc:
+                  "https://images.pexels.com/photos/4350219/pexels-photo-4350219.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                minSalary: 1500,
+                maxSalary: 2300,
+              },
+              {
+                title: "Babysitter",
+                imgSrc:
+                  "https://images.pexels.com/photos/8954794/pexels-photo-8954794.jpeg?auto=compress&cs=tinysrgb&w=600",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "Receptionist",
+                imgSrc:
+                  "https://images.pexels.com/photos/3771811/pexels-photo-3771811.jpeg?auto=compress&cs=tinysrgb&w=600",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "Construction",
+                imgSrc:
+                  "https://images.pexels.com/photos/2219024/pexels-photo-2219024.jpeg?auto=compress&cs=tinysrgb&w=600",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "Gardener",
+                imgSrc:
+                  "https://images.pexels.com/photos/5231143/pexels-photo-5231143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "Packer",
+                imgSrc:
+                  "https://images.pexels.com/photos/4246109/pexels-photo-4246109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+              {
+                title: "Kitchen Assistant",
+                imgSrc:
+                  "https://images.pexels.com/photos/66639/pexels-photo-66639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                minSalary: 1000,
+                maxSalary: 2000,
+              },
+            ]}
+          ></JobRow>
         </div>
       </div>
     </div>
